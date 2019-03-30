ac5f862441c97d18722735a822c85e05bb5532b3
kothawadeakshay <akshaykothawade331@gmail.com>
2019-03-30 15:01:05 +0530

    my last commit

--- src/hello.java
+++ src/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("welcome mavthi");
+		System.out.println("ya basa");
 	}
 
 }
